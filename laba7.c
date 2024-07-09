fd1b8c33cb012eb44ddcab961fb5ae8bf4a83d4b
ziawnclec <ki23-a.zalizniak@nubip.edu.ua>
2024-07-09 22:02:43 +0300

    Add files via upload

---
+++ laba7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x1, y1, r1, x2, y2, r2;
+    printf("Vvedit koordinaty centriv ta radiusi dvoh kil (x1, y1, r1, x2, y2, r2): ");
+    scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &r1, &x2, &y2, &r2);
+
+    double d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    if (d == 0 && r1 == r2) {
+        printf("-1\n");
+    } else if (d > r1 + r2) {
+        printf("0\n");
+    } else if (d == r1 + r2) {
+        printf("1\n");
+    } else if (d < fabs(r1 - r2)) {
+        printf("0\n");
+    } else if (d == fabs(r1 - r2)) {
+        printf("1\n");
+    } else {
+        printf("2\n");
+    }
+
+    return 0;
+}
